4bbfcb3a3eeb15fd71b89bba85770c9c35a43854
liplum <Li_plum@outlook.com>
2025-05-05 10:33:39 +0800

    keep only zh and en l10n

--- apps/nextjs/src/lib/get-dictionary.ts
+++ apps/nextjs/src/lib/get-dictionary.ts
@@ -9,10 +9,6 @@ const dictionaries = {
     import("~/config/dictionaries/en.json").then((module) => module.default),
   zh: () =>
     import("~/config/dictionaries/zh.json").then((module) => module.default),
-  ko: () =>
-    import("~/config/dictionaries/ko.json").then((module) => module.default),
-  ja: () =>
-    import("~/config/dictionaries/ja.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) =>
