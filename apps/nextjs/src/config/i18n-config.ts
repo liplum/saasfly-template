9378ba7c2c55332c614dee1b5d9a99deb17b32be
liplum <Li_plum@outlook.com>
2025-05-05 10:31:43 +0800

    keep only zh and en l10n

--- apps/nextjs/src/config/i18n-config.ts
+++ apps/nextjs/src/config/i18n-config.ts
@@ -1,14 +1,12 @@
 export const i18n = {
   defaultLocale: "zh",
-  locales: ["en", "zh", "ko", "ja"],
-} as const;
+  locales: ["zh", "en"],
+} as const
 
-export type Locale = (typeof i18n)["locales"][number];
+export type Locale = (typeof i18n)["locales"][number]
 
 // 新增的映射对象
 export const localeMap = {
-  en: "English",
   zh: "中文",
-  ko: "한국어",
-  ja: "日本語",
-} as const;
+  en: "English",
+} as const
